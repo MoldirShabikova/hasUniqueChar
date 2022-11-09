3601fcf4e07ec0f01e51022881b661b0ef442a91
moldir <moldirshb@gmail.com>
2022-11-09 10:45:30 -0600

    have to review

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const hasUniqueChars = (word) => {
   
   }
   console.log(hasUniqueChars("Bob"))
+
+  // Write your code below
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    return uniqueChars.size === word.length
+  }
